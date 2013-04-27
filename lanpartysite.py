b4c7e6842f0f1e4a5d211616d3f30e70bff60da8
Sidney Kochman <sidney@kochman.org>
2013-04-27 15:34:16 -0400

    port=port

--- lanpartysite.py
+++ lanpartysite.py
@@ -73,4 +73,4 @@ if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
 	if os.environ.get('PRODUCTION') != "true": app.debug = True
 
-	app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0', port=port)
